659501c4c69dd59c0ceb06270cb2919e234fa63e
Mies van der Lippe <m.vanderlippe@woonz.nl>
2019-07-09 15:59:13 +0200

    Added first unittest

---
+++ Digiwallet.Wrapper.Tests/ApiResponseModels/TransactionStatusResponseModelTest.cs
@@ -0,0 +1,25 @@
+﻿using Digiwallet.Wrapper.Models.TransactionStatus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Digiwallet.Wrapper.Tests.ApiResponseModels
+{
+    [TestClass]
+    public class TransactionStatusResponseModelTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestMethod]
+        public void OkResponse()
+        {
+            var input = "000000 OK";
+            var model = new TransactionStatusResponseModel(input);
+
+            Assert.AreEqual(TransactionStatusResponseCodes.Ok, model.Status, "Didn't find Ok statuscode");
+            Assert.AreEqual("000000", model.StatusCode, "Didn't correctly map API statuscode to string");
+            Assert.AreEqual("OK", model.ResponseBody, "Didn't parse API response text correctly.");
+        }
+    }
+}
