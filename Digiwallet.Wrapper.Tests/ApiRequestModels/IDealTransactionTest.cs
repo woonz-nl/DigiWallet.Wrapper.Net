75d0c7dc8e150a03c657cb05ff5dfb4e2a2955a2
Mies van der Lippe <m.vanderlippe@woonz.nl>
2019-07-18 13:51:51 +0200

    Polished up current unittests
    Got feedback on styling of unit tests. Updated them to match MSDN guidelines;
    https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-best-practices

--- Digiwallet.Wrapper.Tests/ApiRequestModels/IDealTransactionTest.cs
+++ Digiwallet.Wrapper.Tests/ApiRequestModels/IDealTransactionTest.cs
@@ -1,24 +1,29 @@
 ﻿using Digiwallet.Wrapper.Models.Transaction;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Digiwallet.Wrapper.Tests.ApiRequestModels
 {
     [TestClass]
     public class IDealTransactionTest
     {
+        /// <summary>
+        /// Due to SEPA transaction description limitations, one cannot use any characters other than alphabetic and numeric. 
+        /// This test is very well suited to be converted to a xUnit Theory instead, with multiple inputs / outputs. 
+        /// </summary>
         [TestMethod]
-        public void DescriptionLimitationsTest()
+        public void InvalidInput_Filter_ToValidString()
         {
-            var inputString = "You# (@Sho))(uld b(*!@#e ab__~++~_le )(2!(*$#)(*)) r@@@@@@@@ea@@@@@d ______th_____is______ 1*&)(&2)(*()$@!&(*3)*($)!@*(@";
+            // Arrange
+            const string inputString = "You# (@Sho))(uld b(*!@#e ab__~++~_le )(2!(*$#)(*)) r@@@@@@@@ea@@@@@d ______th_____is______ 1*&)(&2)(*()$@!&(*3)*($)!@*(@";
+            const string expectedOutput = "You Should be able 2 read this 1";
 
-            var iDealTransaction = new IDealTransaction() {
+            // Act (calls setter)
+            IDealTransaction iDealTransaction = new IDealTransaction() {
                 Description = inputString
             };
 
-            Assert.AreEqual("You Should be able 2 read this 1", iDealTransaction.Description, "Didn't filter input correctly.");
+            // Assert
+            Assert.AreEqual(expectedOutput, iDealTransaction.Description, "Didn't filter input correctly.");
             Assert.AreEqual(32, iDealTransaction.Description.Length, "Failed to limit charcount to 32.");
         }
     }
--- Digiwallet.Wrapper.Tests/ApiResponseModels/TransactionStatusResponseModelTest.cs
+++ Digiwallet.Wrapper.Tests/ApiResponseModels/TransactionStatusResponseModelTest.cs
@@ -12,33 +12,45 @@ namespace Digiwallet.Wrapper.Tests.ApiResponseModels
         }
 
         [TestMethod]
-        public void OkResponse()
+        public void Parse_Ok_TransactionStatus()
         {
-            var input = "000000 OK";
-            var model = new TransactionStatusResponseModel(input);
+            // Arrange
+            const string input = "000000 OK";
 
+            // Act
+            TransactionStatusResponseModel model = new TransactionStatusResponseModel(input);
+
+            // Assert
             Assert.AreEqual(TransactionStatusResponseCodes.Ok, model.Status, "Didn't find Ok statuscode");
             Assert.AreEqual("000000", model.StatusCode, "Didn't correctly map API statuscode to string");
             Assert.AreEqual("OK", model.ResponseBody, "Didn't parse API response text correctly.");
         }
 
         [TestMethod]
-        public void NotCompletedResponse()
+        public void Parse_NotCompleted_TransactionStatus()
         {
-            var input = "DW_SE_0020 Transaction has not been completed, try again later";
-            var model = new TransactionStatusResponseModel(input);
+            // Arrange
+            const string input = "DW_SE_0020 Transaction has not been completed, try again later";
 
+            // Act
+            TransactionStatusResponseModel model = new TransactionStatusResponseModel(input);
+
+            // Assert
             Assert.AreEqual(TransactionStatusResponseCodes.NotCompleted, model.Status, "Didn't find NotCompleted statuscode");
             Assert.AreEqual("DW_SE_0020", model.StatusCode, "Didn't correctly map API statuscode to string");
             Assert.AreEqual("Transaction has not been completed, try again later", model.ResponseBody, "Didn't parse API response text correctly.");
         }
 
         [TestMethod]
-        public void ValidationFailedResponse()
+        public void Parse_ValidationFailed_TransactionStatus()
         {
-            var input = "DW_XE_0003 Validation failed, details: {\"outletID\":[\"Outlets above 62239 cannot be even.\"]}";
-            var model = new TransactionStatusResponseModel(input);
+            // Arrange
+            const string input = "DW_XE_0003 Validation failed, details: {\"outletID\":[\"Outlets above 62239 cannot be even.\"]}";
 
+            // Act
+            TransactionStatusResponseModel model = new TransactionStatusResponseModel(input);
+
+            // Assert
             Assert.AreEqual(TransactionStatusResponseCodes.ValidationFailed, model.Status, "Didn't find ValidationFailed statuscode");
             Assert.AreEqual("DW_XE_0003", model.StatusCode, "Didn't correctly map API statuscode to string");
         }
